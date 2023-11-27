194017347dce3fd3f83b7e1dd6a95d1027172154
huy nguyen <nguyenviethuy181201@gmail.com>
2023-11-27 18:54:04 +0700

    delete all

--- server/config/vnpay.js
+++
@@ -1,7 +0,0 @@
-export default{
-    "vnp_TmnCode":"LTE7U5NH",
-    "vnp_HashSecret":"KOERYIIPSUIGJGNWCYTMLMCZTVHQPEDM",
-    "vnp_Url":"https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
-    "vnp_Api":"https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
-    "vnp_ReturnUrl": "http://localhost:4000/api/vnpay/return/",
-  }
